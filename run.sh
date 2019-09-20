cb864e0a9f454702355fc59bc51339d178279e7c
uday kiran <kiran4frndz@gmail.com>
2019-09-20 13:39:37 +0530

    added vim

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-sudo apt install zsh curl wget gcc git npm
+sudo apt install zsh curl wget gcc git npm neovim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+curl -sLf https://spacevim.org/install.sh | bash
 
